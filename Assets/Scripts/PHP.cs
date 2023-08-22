5e4e299f460944222b55337c0138b73b83414c02
Vilius Ivanovas <ivanovasvilius@gmail.com>
2023-08-22 09:02:47 +0200

    Setting up some unity code to post your high score

---
+++ Assets/Scripts/PHP.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PHP : MonoBehaviour
+{
+	public string URL;
+	public WWW WWW;
+	
+	void Start() {
+		StartCoroutine(Upload());
+	}
+
+	IEnumerator Upload(){
+		yield return Upload1();
+		yield return Upload2();
+	}
+
+	IEnumerator Upload1() {
+		List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+		formData.Add( new MultipartFormDataSection("field1=foo&field2=bar") );
+		//formData.Add( new MultipartFormFileSection("my file data", "myfile.txt") );
+
+		UnityWebRequest www = UnityWebRequest.Post(url, formData);
+		yield return www.SendWebRequest();
+
+		if(www.isNetworkError || www.isHttpError) {
+			Debug.Log(www.error);
+		}
+		else {
+			Debug.Log("Form upload complete!");
+			Debug.Log( "MULTIPART: " + www.downloadHandler.text );
+		}
+	}
+
+	IEnumerator Upload2() {
+		WWWForm form = new WWWForm();
+		form.AddField("myField", "myData");
+
+		UnityWebRequest www = UnityWebRequest.Post(url, form);
+		yield return www.SendWebRequest();
+
+		if(www.isNetworkError || www.isHttpError) {
+			Debug.Log(www.error);
+		}
+		else {
+			Debug.Log("Form upload complete!");
+			Debug.Log( "WWWForm: " + www.downloadHandler.text );
+		}
+	}
+	}
+}
